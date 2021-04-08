39e65527506aad20e0292fcb4200d49ecc94d9e8
judyken29 <81622421+judyken29@users.noreply.github.com>
2021-04-08 12:26:00 +0530

    Create CarApp.java

---
+++ CarApp.java
@@ -0,0 +1,25 @@
+package classs;
+
+public class CarApp {
+	
+public static void main(String[] args) {
+		
+		
+		Car c1 = new Car();
+		int x=10;
+		
+		c1.setModel("Ford");
+		c1.setMaxspeed(9);
+		c1.setYear(10);
+		System.out.println(x);
+		System.out.println(c1);
+		
+		Car c2 = new Car();
+		c2.startcar();
+		
+		
+		
+		
+	}
+
+}
